5c1ea16f78b6ae38b6be83a42af14aabfc1ed1d7
jonathastavares <jonathastavares@id.uff.br>
2021-07-23 12:14:53 -0300

    Added Unit test for User associations

---
+++ spec/models/integrations_spec.rb
@@ -0,0 +1,8 @@
+require 'rails_helper'
+require 'capybara/rails'
+# rubocop:disable Lint/UselessAssignment
+RSpec.describe 'Integrations', type: :system do
+  describe 'Timeline' do
+  end
+end
+# rubocop:enable Lint/UselessAssignment
---
+++ spec/models/user_model_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+RSpec.describe User, type: :model do
+  context 'User/Entities relations' do
+    it 'Checks associations between User and Entities' do
+      user = User.create!(username:'testuser')
+      entity = Entity.create!(name:'testentity', amount:'0', user_id: user.id)
+      expect(user.entities).to eq([entity])
+    end
+  end
+  context 'User/Groups relations' do
+    it 'Checks associations between User and Groups' do
+      user = User.create!(username:'testuser')
+      group = Group.create!(name:'testgroup', user_id: user.id)
+      expect(user.groups).to eq([group])
+    end
+  end
+end
