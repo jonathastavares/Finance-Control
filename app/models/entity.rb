cabb5b0acdc54d247af89e5bbcf9c3fd1d9297a3
jonathastavares <jonathastavares@id.uff.br>
2021-07-23 12:25:46 -0300

    Added Unit tests for Entity and Group models associations

--- app/models/entity.rb
+++ app/models/entity.rb
@@ -1,6 +1,6 @@
 class Entity < ApplicationRecord
   belongs_to :creator, class_name: 'User', foreign_key: 'user_id'
-  has_many :groups
+  belongs_to :group, class_name: 'Group', foreign_key: 'group_id', optional: true
   validates :amount, presence: true
   validates :name, presence: true
 end
